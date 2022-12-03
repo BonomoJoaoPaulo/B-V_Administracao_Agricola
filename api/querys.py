9588866af36013e1b90908280430845eb4054c81
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-12-03 16:15:37 -0300

    Fixed endpoint POST

--- api/querys.py
+++ api/querys.py
@@ -26,7 +26,7 @@ def get_all_propertys():
     return propertys_list
 
 def get_property_cost(property_name):
-    query = """SELECT p.ID_propriedade, rdc.* FROM Propriedade as p 
+    query = f"""SELECT p.ID_propriedade, rdc.* FROM Propriedade as p 
         JOIN Registro_custo_prop as rcp ON p.ID_propriedade = rcp.ID_propriedade
         JOIN Registro_de_Custo as rdc ON rcp.ID_registro_custo = rdc.ID_registro_custo
         WHERE p.nome = {property_name}"""
@@ -45,7 +45,7 @@ def get_property_cost(property_name):
             cost_from_property_list.append(value)
 
 def get_property_workers(ID_propriedade):
-    query = """ SELECT p.nome, f.* FROM Propriedade as p
+    query = f""" SELECT p.nome, f.* FROM Propriedade as p
         JOIN Funcionario as f ON p.ID_propriedade = f.ID_propriedade
         WHERE p.ID_propriedade = int{ID_propriedade}
     """
@@ -64,7 +64,7 @@ def get_property_workers(ID_propriedade):
             property_workers_list.append(value)
 
 def get_property_plantations(ID_propriedade):
-    query = """ SELECT p.nome, c.nome, c.area_cultuvada FROM Propriedade as p
+    query = f""" SELECT p.nome, c.nome, c.area_cultuvada FROM Propriedade as p
     JOIN Produz_prop_cult as ppc ON ppc.ID_propriedade = p.ID_propriedade
     JOIN Cultura as c ON ppc.ID_cultura = c.ID_cultura
     WHERE p.ID_propriedade = {ID_propriedade}
@@ -84,7 +84,7 @@ def get_property_plantations(ID_propriedade):
             property_plantation.append(value)
 
 def get_culture_cost(ID_cultura):
-    query = """ SELECT c.nome, c.area_cultivada, rdc.tipo_custo, rdc.descricao FROM Cultura as c
+    query = f""" SELECT c.nome, c.area_cultivada, rdc.tipo_custo, rdc.descricao FROM Cultura as c
     JOIN Registro_custo_cult as rcc ON c.ID_cultura = rcc.ID_cultura
     JOIN Registro_de_custo as rdc ON rcc.ID_registro_custo = rdc.ID_registro_custo
     WHERE rdc.cultura_destinada = {ID_cultura}
